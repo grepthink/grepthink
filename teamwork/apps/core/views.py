a89f9530582903e0590b7df0ffe62558e027bc10
Sean Dougher <hi.imsean@yahoo.com>
2017-02-11 14:54:50 -0800

    Added 'create_project' to views

--- teamwork/apps/core/views.py
+++ teamwork/apps/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 
 def home(request):
     return render(request, 'core/home.html')
+
+def create_project(request):
+    return render(request, 'core/create_project.html')
