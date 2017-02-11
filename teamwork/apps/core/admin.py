0c6fe9b9e13992fedddac7e39480815394e4f281
Ryan Monroe <rmonroe@ucsc.edu>
2017-02-10 19:26:16 -0800

    User and Project Table

--- teamwork/apps/core/admin.py
+++ teamwork/apps/core/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Course
+from .models import Project
+from .models import User
 
 admin.site.register(Course)
+admin.site.register(Project)
+admin.site.register(User)
