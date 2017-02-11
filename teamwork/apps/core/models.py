f0d9645949e0a60657df778f301a1edc4e37d3b7
Ryan Monroe <rmonroe@ucsc.edu>
2017-02-10 16:15:06 -0800

    add course table for DB

--- teamwork/apps/core/models.py
+++ teamwork/apps/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+	course_name = models.CharField(max_length=30)
+
+#class Project(models.Model):
+
+#class User(models.Model):
--- teamwork/settings.py
+++ teamwork/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'teamwork.apps.core',
+
 ]
 
 MIDDLEWARE_CLASSES = [
