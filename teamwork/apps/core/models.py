ca77a334065232e12c6df8b0ae1ca0ae339fefc7
Michael Gates <mjgates@ucsc.edu>
2017-02-11 21:59:55 -0800

    Made some changes to models.py in testing

--- teamwork/apps/core/models.py
+++ teamwork/apps/core/models.py
@@ -1,33 +1,27 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from django.db import models
 
 # Import something from django for user auth
 
-"""
-The main models of the core app.
+#The main models of the core app.
 
-Course: A database model (object) for courses
-
-
-
-"""
-
-class
+#Course: A database model (object) for courses
 
 class Course(models.Model):
 	course_name = models.CharField(max_length=30, default="general")
 	course_info = models.CharField(max_length=100, default="")
+	# Not tested
 	institution = models.CharField(max_length=100, default="collab")
-	term = CharField(max_length=100, default="It's alsways Spring")
-	year = models.integer()
+	term = models.CharField(max_length=100, default="It's alsways Spring")
+	#year = models.DateTime()
 
 
 	def __str__(self):
 		return self.course_name
-
-		do that class meta thing to add attributes
 
 class User(models.Model):
 	user_first = models.CharField(max_length=20, default="")
@@ -35,20 +29,18 @@ class User(models.Model):
 		return self.user_first
 
 class Project(models.Model):
-"""
-Authors is a is a forign key from class User.
-Each project object is associated with authors (current_members).
+#Authors is a is a forign key from class User.
+#Each project object is associated with authors (current_members).
 
-"""
 	# Authors
 	authors = models.ForeignKey(User, related_name='+')
-
 	# all many to many fields first, to easily identify relationships
 	course = models.ManyToManyField(Course, related_name="course_id")
-	project_owner = models.ManyToManyField(User, related_name="po")
+	#project_owner = models.ManyToManyField(User, related_name="po")
 
 	project_name = models.CharField(max_length=50, default="")
 	project_info = models.CharField(max_length=100, default="")
+	create_date = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.project_name
