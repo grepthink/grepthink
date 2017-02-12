76598ff703de80a824e4448c90cdd49946780bba
Michael Gates <mjgates@ucsc.edu>
2017-02-11 19:31:36 -0800

    made some changes to models.py

--- teamwork/apps/core/models.py
+++ teamwork/apps/core/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Import something from django for user auth
+
+"""
+The main models of the core app.
+
+Course: A database model (object) for courses
+
+
+
+"""
+
+class 
 
 class Course(models.Model):
-	course_name = models.CharField(max_length=30, default="")
+	course_name = models.CharField(max_length=30, default="general")
 	course_info = models.CharField(max_length=100, default="")
+	institution = models.CharField(max_length=100, default="collab")
+	term = CharField(max_length=100, default="It's alsways Spring")
+	year = models.integer()
+
+
 	def __str__(self):
 		return self.course_name
+
+		do that class meta thing to add attributes
 
 class User(models.Model):
 	user_first = models.CharField(max_length=20, default="")
@@ -22,7 +40,3 @@ class Project(models.Model):
 	project_info = models.CharField(max_length=100, default="")
 	def __str__(self):
 		return self.project_name
-
-
-
-
