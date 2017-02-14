b551f94d08b30c8859e2c68b0684290a54995612
Michael Gates <mjgates@ucsc.edu>
2017-02-13 18:14:15 -0800

    Create and view projects working

--- teamwork/apps/core/views.py
+++ teamwork/apps/core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from .models import *
 from .forms import *
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
 
@@ -22,6 +25,7 @@ def create_course(request):
         #form.save()
     return render(request, 'core/create_project.html', {'form': form})
 """
+"""
 #Does not create a project because we dont know how to use many-to-many
 def create_project(request):
     #If post request we need to process form data
@@ -42,3 +46,45 @@ def view_projects(request):
     context = { 'project_name': project_name,
     }
     return render(request, 'core/view_projects.html', context)
+
+"""
+
+def _projects(request, projects):
+    """
+    Private method that will be used for paginator once I figure out how to get it working.
+    """
+    #paginator = Paginator(projects, 10)
+    page = request.GET.get('page')
+    #try:
+    #    projects = paginator.page(page)
+    #except PageNotAnInteger:
+    #    projects = paginator.page(1)
+    #except EmptyPage:
+    #    projects = paginator.page(paginator.num_pages)
+    return render(request, 'core/view_projects.html', {
+        'projects': projects,
+    })
+
+def view_projects(request):
+    """
+    Public method that takes a request, retrieves all Project objects from the model, 
+    then calls _projects to render the request to template view_projects.html
+    """
+    all_projects = Project.get_published()
+    return _projects(request, all_projects)
+
+def create_project(request):
+    """
+    Public method that creates a form and renders the request to create_project.html
+    """
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = Project()
+            project.title = form.cleaned_data.get('title')
+            project.save()
+            return redirect('/view_projects.html/')
+    else:
+        form = ProjectForm()
+    return render(request, 'core/create_project.html', {'form': form})
+
