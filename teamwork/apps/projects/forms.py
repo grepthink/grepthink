f892be12b7ad7fe54bea1e8833eb56bffadf7389
Sean Dougher <hi.imsean@yahoo.com>
2017-02-28 14:41:04 -0800

    Only professor creates course/only professor can sponsor projects

--- teamwork/apps/projects/forms.py
+++ teamwork/apps/projects/forms.py
@@ -8,11 +8,16 @@ class ProjectForm(forms.ModelForm):
 		super(ProjectForm, self).__init__(*args, **kwargs)
 		# exclude the current user and the superuser
 		self.fields['members'].queryset = User.objects.exclude(is_superuser=True)
+		if not User.objects.get(id=uid).profile.isProf:
+			self.fields['sponsor'].widget = forms.HiddenInput()
 
+	#tp = User.objects.get(id=uid)
 	title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),max_length=255)
 	members = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple,queryset=User.objects.all(), required=False)
 	accepting = forms.BooleanField(initial= True, label = 'accepting members', required = False)
+	#if tp.profile.isProf:
 	sponsor = forms.BooleanField(initial = False, label = 'Sponsored?', required = False)
+
 	class Meta:
 	    model = Project
 	    fields = ['title']
