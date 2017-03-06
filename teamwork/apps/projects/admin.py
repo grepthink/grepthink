259e7467c9e249bfb7f4fa095f0a58cd4916e243
Michael Gates <mjgates@ucsc.edu>
2017-03-05 20:48:02 -0800

    Still runs, new migrations from professor bool rename

--- teamwork/apps/projects/admin.py
+++ teamwork/apps/projects/admin.py
@@ -5,10 +5,10 @@ from django.contrib import admin
 from .models import Project
 from .models import Membership
 from .models import ProjectUpdate
-from .models import Intrest
+from .models import Interest
 
 # Register the following models for the admin site
 admin.site.register(Project)
 admin.site.register(Membership)
 admin.site.register(ProjectUpdate)
-admin.site.register(Intrest)
+admin.site.register(Interest)
