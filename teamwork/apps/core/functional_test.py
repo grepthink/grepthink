c22188570a7cd2f1c1cc5033fab5d7cf8dd620b5
Ryan Monroe <rmonroe@ucsc.edu>
2017-04-19 01:03:26 -0700

    some tests

---
+++ teamwork/apps/core/functional_test.py
@@ -0,0 +1,22 @@
+#from django.test import TestCase
+
+# Create your tests here.
+
+
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_load_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Teamwork | Teamwork', self.browser.title)
+        
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
