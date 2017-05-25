7e5e1c876d4b1440ee4d4e19d48756098221c171
August Valera <4U6U57@gmail.com>
2017-05-24 20:04:01 -0700

    Fix merge errors, it compiles, ready to merge

--- manage.py
+++ manage.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    if sys.argv[1].lower() in ["gt", "groupthink", "grepthink"]:
+        for cmd in ["makemigrations", "migrate", "runserver"]:
+            sys.argv[1] = cmd
+            execute_from_command_line(sys.argv)
+    else:
+        execute_from_command_line(sys.argv)
