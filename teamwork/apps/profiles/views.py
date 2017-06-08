d462a99fc6f80e4a23f4af5c18b451355527c215
Michael Gates <mjgates@ucsc.edu>
2017-06-08 01:43:43 -0700

    Add JsonRespons import for edit_profile JS

--- teamwork/apps/profiles/views.py
+++ teamwork/apps/profiles/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import (HttpResponse, HttpResponseBadRequest,
-                         HttpResponseRedirect)
+                         HttpResponseRedirect, JsonResponse)
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
