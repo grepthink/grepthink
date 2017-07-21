725cbe85939019ae449508fbbb8c890fca3b02d1
Kunal <kusdhill@ucsc.edu>
2017-07-20 20:35:39 -0700

    undid timezone change

--- teamwork/settings.py
+++ teamwork/settings.py
@@ -147,7 +147,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = "America/Los_Angeles"
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
