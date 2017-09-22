9262e2404415e3e8ea7b917313705991189297e5
Kevin Pannell <pannellk69@yahoo.com>
2017-09-22 01:29:53 -0700

    export course to spreadsheet, fixed prof showing in stats page

--- teamwork/apps/courses/views.py
+++ teamwork/apps/courses/views.py
@@ -154,7 +154,7 @@ def view_stats(request, slug):
     elif not request.user.profile.isProf:
         return redirect(view_one_course, cur_course.slug)
 
-    students_num = Enrollment.objects.filter(course = cur_course)
+    students_num = Enrollment.objects.filter(course = cur_course, role="student")
     projects_num = projects_in_course(slug)
     students_projects = []
     students_projects_not = []
@@ -792,5 +792,91 @@ def upload_csv(request, slug):
 
     return render(request, 'core/upload_csv.html', {
         'slug':slug, 'form':form, 'course':course,
+        'page_name':page_name, 'page_description':page_description,'title':title
+    })
+
+import xlwt
+@login_required
+def export_xls(request, slug):
+    page_name = "Export Course"
+    page_description = "Save the current course's projects and associated members in an excel spreadsheet"
+    title = "Export Course"
+    course = get_object_or_404(Course, slug=slug)
+
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="course_overview.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet(course.name)
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    # header row
+    ws.write(row_num, 0, course.name, font_style)
+    ws.write(row_num, 1, course.term, font_style)
+    ws.write(row_num, 2, course.year, font_style)
+
+    projects = Project.objects.filter(course=course)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    for proj in projects:
+        row_num += 1
+        members = proj.get_members()
+        ws.write(row_num, 0, proj.title, font_style)
+        row_num += 1
+        ws.write(row_num, 1, "TA Meeting:", font_style)
+        row_num += 1
+        ws.write(row_num, 1, "Location:", font_style)
+        for mem in members:
+            row_num += 1
+            ws.write(row_num, 1, mem.email, font_style)
+
+    students_num = Enrollment.objects.filter(course = course, role="student")
+    projects_num = projects_in_course(course.slug)
+    students_projects = []
+    students_projects_not = []
+
+    for i in projects_num:
+        for j in i.members.all():
+            if not j in students_projects:
+                students_projects.append(j)
+
+    for i in students_num:
+        if not i.user in students_projects:
+            students_projects_not.append(i.user)
+
+    row_num += 3
+    ws.write(row_num, 0, "Project-less", font_style)
+    for stud in students_projects_not:
+        row_num += 1
+        ws.write(row_num, 1, stud.email, font_style)
+
+
+
+
+    # columns = ['Course', 'First name', 'Last name', 'Email address', ]
+    # for col_num in range(len(columns)):
+    #     ws.write(row_num, col_num, columns[col_num], font_style)
+    #
+    # # Sheet body, remaining rows
+    # font_style = xlwt.XFStyle()
+    #
+    # rows = User.objects.all().values_list('username', 'first_name', 'last_name', 'email')
+    # for row in rows:
+    #     row_num += 1
+    #     for col_num in range(len(row)):
+    #         ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
+
+    return render(request, 'courses/export.html', {
+        'course':course,
         'page_name':page_name, 'page_description':page_description,'title':title
     })
