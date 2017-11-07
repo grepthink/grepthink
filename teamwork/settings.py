58caaee8e68f8178328710abfc4f6f9d8f114593
Michael Gates <mjgates@ucsc.edu>
2017-11-07 02:28:59 -0800

    Changes static storage to django built in, over whitenoise

--- teamwork/settings.py
+++ teamwork/settings.py
@@ -197,8 +197,9 @@ MEDIA_URL = '/media/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 ALLOWED_SIGNUP_DOMAINS = ['*']
 
