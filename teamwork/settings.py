9d416e019eea5c028b580a359f9209d6e050a7ff
Trevor Ching <ttching@ucsc.edu>
2017-11-23 12:22:43 -0800

    Default db restored

--- teamwork/settings.py
+++ teamwork/settings.py
@@ -152,7 +152,7 @@ else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
     }
 }
 
