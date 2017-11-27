a4b76bc3d5e8de1df8bc07f5d78deca2d020ec0d
Tyler Schmidt <tmschmid@ucsc.edu>
2017-11-26 17:26:02 -0800

    - Updated ChartJS to 2.7.1. Converted old naming conventions to new ones. - Made Student Contributions Graph to Horizontal Bar Graph.

--- teamwork/static/js/student_contribution_received.js
+++ teamwork/static/js/student_contribution_received.js
@@ -15,7 +15,7 @@ for(let i = 0; i < member_length; i++) {
     colors_array.push(getRandomColor());
 }
 
-function makeBarChart() {
+function make_local_barchart() {
     let member_contributions = JSON.parse(document.getElementById('contributions_of_members').value)
     let tsr_number = parseInt(document.getElementById('TSR #').value) - 1;
     let number_of_members = document.getElementById('Member').options.length
@@ -25,59 +25,76 @@ function makeBarChart() {
         local_labels.push(current_member);
         document.getElementById(current_member).style = "background-color:" + colors_array[i];
     }
-    let barChartOptions = {
-        //Boolean - Whether we should show a stroke on each segment
-        barShowStroke : true,
-        scaleOverride: true,
-        scaleSteps: 10,
-        scaleStepWidth: 10,
-        scaleStartValue: 0,
-        scaleLineColor: "rgba(0,0,0,.1)",
-        scaleLineWidth: 1,
-        scaleShowLabels: true,
-        scaleLabel: "<%=value%>",
-        scaleFontFamily: "'Arial'",
-        scaleFontSize: 12,
-        scaleFontStyle: "normal",
-        scaleFontColor: "#666",
-        scaleShowGridLines: !0,
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        scaleGridLineWidth: 1,
-
-        //Number - The width of each segment stroke
-        barStrokeWidth : 2,
-
-        //Boolean - Whether we should animate the chart
-        animation : true,
-
-        //Number - Amount of animation steps
-        animationSteps : 100,
-
-        //String - Animation easing effect
-        animationEasing : "easeOutBounce",
-    };
 
     let barChartData = {
         labels: local_labels,
         datasets: []
     };
+
     for(let i = 0; i < number_of_members; i++) {
         barChartData.datasets.push(
         {
-            fillColor: colors_array[i],
-            strokeColor: 'black',
+            label: local_labels[i],
+            backgroundColor: colors_array[i],
+            borderColor: 'black',
             borderWidth: 1,
             data: member_contributions[i][tsr_number]
-        }
-        )
+        });
     }
 
     var ctx = document.getElementById("local_student_contribution").getContext("2d");
-    try {
-        new Chart(ctx).Bar(barChartData, barChartOptions)
-    } catch(myError) {
-        console.log(myError.message)
+    window.my_local_barchart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: barChartData,
+        options: {
+            // Elements options apply to all of the options unless overridden in a dataset
+            // In this case, we are setting the border of each horizontal bar to be 2px wide
+            elements: {
+                rectangle: {
+                    borderWidth: 2,
+                }
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 100,
+                        stepSize: 10
+                    }
+                }]
+            },
+            title: {
+                display: true,
+                text: "Students' Contribution Scores Received"
+            }
+        }
+    });
+}
+
+function update_local_barchart() {
+    let member_contributions = JSON.parse(document.getElementById('contributions_of_members').value)
+    let tsr_number = parseInt(document.getElementById('TSR #').value) - 1;
+    let number_of_members = document.getElementById('Member').options.length
+    var local_labels = [];
+    for(let i = 0; i < number_of_members; i++) {
+        let current_member = document.getElementById('Member').options[i].text;
+        local_labels.push(current_member);
+        document.getElementById(current_member).style = "background-color:" + colors_array[i];
     }
+
+    my_local_barchart.data.datasets = [];
+    for(let i = 0; i < number_of_members; i++) {
+        my_local_barchart.data.datasets.push(
+        {
+            label: local_labels[i],
+            backgroundColor: colors_array[i],
+            borderColor: 'black',
+            borderWidth: 1,
+            data: member_contributions[i][tsr_number]
+        });
+    }
+    window.my_local_barchart.update();
 }
 
 function make_historical_barchart() {
@@ -90,37 +107,6 @@ function make_historical_barchart() {
     }
     //let member_contributions = JSON.parse(document.getElementById('contributions_of_members').value)
     let historical_averages = JSON.parse(document.getElementById('member_averages').value)
-    let barChartOptions = {
-        //Boolean - Whether we should show a stroke on each segment
-        barShowStroke : true,
-        scaleOverride: true,
-        scaleSteps: 10,
-        scaleStepWidth: 10,
-        scaleStartValue: 0,
-        scaleLineColor: "rgba(0,0,0,.1)",
-        scaleLineWidth: 1,
-        scaleShowLabels: true,
-        scaleLabel: "<%=value%>",
-        scaleFontFamily: "'Arial'",
-        scaleFontSize: 12,
-        scaleFontStyle: "normal",
-        scaleFontColor: "#666",
-        scaleShowGridLines: !0,
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        scaleGridLineWidth: 1,
-
-        //Number - The width of each segment stroke
-        barStrokeWidth : 2,
-
-        //Boolean - Whether we should animate the chart
-        animation : true,
-
-        //Number - Amount of animation steps
-        animationSteps : 100,
-
-        //String - Animation easing effect
-        animationEasing : "easeOutBounce",
-    };
     var last_element = tsr_numbers_array.pop();
     let my_data_array = []
     for(let i = 0; i < last_element; i++) {
@@ -129,15 +115,72 @@ function make_historical_barchart() {
     let barChartData = {
             labels: historical_labels,
             datasets: [{
-                fillColor: colors_array[current_student_index],
-                strokeColor: 'black',
+                label: current_student,
+                backgroundColor: colors_array[current_student_index],
+                borderColor: 'black',
                 borderWidth: 1,
                 data: my_data_array
             }]
     };
-    let ctx = document.getElementById("historical_student_contribution").getContext("2d");
-    new Chart(ctx).Bar(barChartData, barChartOptions)
+
+    var ctx = document.getElementById("historical_student_contribution").getContext("2d");
+    window.my_historical_barchart = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            elements: {
+                rectangle: {
+                    borderWidth: 2,
+                }
+            },
+            responsive: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 100,
+                        stepSize: 10
+                    }
+                }]
+            },
+            title: {
+                display: true,
+                text: "Historical Averages"
+            }
+        }
+    });
 }
 
-makeBarChart();
+function update_historical_barchart() {
+    my_historical_barchart.data = [];
+
+    let tsr_numbers_array = JSON.parse(document.getElementById('tsr_numbers').value)
+    let current_student = document.getElementById('Member').value;
+    let current_student_index = document.getElementById('Member').selectedIndex;
+    let historical_labels = [];
+    for(let i = 0; i < tsr_numbers_array.length; i++) {
+        historical_labels.push("TSR #" + tsr_numbers_array[i]);
+    }
+
+    let historical_averages = JSON.parse(document.getElementById('member_averages').value)
+    var last_element = tsr_numbers_array.pop();
+    let member_historical_averages = [];
+    for(let i = 0; i < last_element; i++) {
+        member_historical_averages.push(historical_averages[i][current_student_index]);
+    }
+    let barChartData = {
+            labels: historical_labels,
+            datasets: [{
+                label: current_student,
+                backgroundColor: colors_array[current_student_index],
+                borderColor: 'black',
+                borderWidth: 1,
+                data: member_historical_averages
+            }]
+    };
+    my_historical_barchart.data = barChartData;
+    window.my_historical_barchart.update();
+}
+
+make_local_barchart();
 make_historical_barchart();
