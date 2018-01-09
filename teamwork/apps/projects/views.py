ebdf27c324a448f99107f6d9806e6c64beac2da1
Kevin Pannell <kapannel@ucsc.edu>
2018-01-08 21:27:22 -0800

    Changed Project promotion buttons to dropdowns.

--- teamwork/apps/projects/views.py
+++ teamwork/apps/projects/views.py
@@ -526,8 +526,6 @@ def edit_project(request, slug):
         messages.warning(request, 'Only the Project Owner can make changes to this project!')
         return redirect(view_one_project, project.slug)
 
-    print("WE IN HERE")
-    print(request.method)
     if request.POST.get('delete_project'):
         print("deleting project")
         # Rights: GT, Professor, TA, Project Creator
@@ -581,34 +579,6 @@ def edit_project(request, slug):
             messages.add_message(request, messages.SUCCESS, "Greppers have been invited to join your project!")
         else:
             messages.add_message(request, messages.WARNING, "Failed to invite member(s) to project")
-
-        # curr_members = Membership.objects.filter(project=project)
-        #
-        # # Create membership objects for the newly added members
-        # for uname in members:
-        #     mem_to_add = User.objects.get(username=uname)
-        #     mem_courses = Course.get_my_courses(mem_to_add)
-        #     # Don't add a member if they already have membership in project
-        #     # Confirm that the member is a part of the course
-        #     # List comprehenshion: loops through this projects memberships in order
-        #     #   to check if mem_to_add is in the user field of a current membership.
-        #     if this_course in mem_courses and mem_to_add not in [mem.user for mem in curr_members]:
-        #         Membership.objects.create(
-        #             user=mem_to_add, project=project, invite_reason='')
-        #         Alert.objects.create(
-        #             sender=request.user,
-        #             to=mem_to_add,
-        #             msg="You were added to " + project.title,
-        #             url=reverse('view_one_project',args=[project.slug]),
-        #             )
-        #         # remove member from pending list if he/she was on it
-        #         pending_members = project.pending_members.all()
-        #         if mem_to_add in pending_members:
-        #             for mem in pending_members:
-        #                 if mem == mem_to_add:
-        #                     project.pending_members.remove(mem)
-        #                     project.save()
-
 
         return redirect(view_one_project, project.slug)
 
@@ -692,7 +662,7 @@ def edit_project(request, slug):
         return redirect(edit_project, slug)
 
     if request.method == 'POST':
-        form = EditProjectForm(request.user.id, request.POST)
+        form = EditProjectForm(request.user.id, request.POST, members=members)
 
         if form.is_valid():
             # edit the project object, omitting slug
@@ -707,6 +677,11 @@ def edit_project(request, slug):
             project.project_image = form.cleaned_data.get('project_image')
             project.ta_time = form.cleaned_data.get('ta_time')
             project.ta_location = form.cleaned_data.get('ta_location')
+
+            # roles
+            project.creator = form.cleaned_data.get('project_owner')
+            project.scrum_master = form.cleaned_data.get('scrum_master')
+
             # Project content
             project.content = form.cleaned_data.get('content')
             project.lower_time_bound = form.cleaned_data.get('lower_time_bound')
@@ -717,7 +692,7 @@ def edit_project(request, slug):
             # Not sure if view_one_project redirect will work...
             return redirect(view_one_project, project.slug)
     else:
-        form = EditProjectForm(request.user.id, instance=project)
+        form = EditProjectForm(request.user.id, instance=project, members=members)
     return render(request, 'projects/edit_project.html', {'page_name': page_name,
         'page_description': page_description, 'title' : title, 'members':members,
         'form': form, 'project': project, 'user':request.user})
