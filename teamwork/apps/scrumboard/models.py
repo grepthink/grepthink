52066ff4a23875a982fe610b9b93bb41df1de0c4
hwhh <hwhh@st-andrews.ac.uk>
2018-02-10 19:26:15 -0800

    cleaned up

--- teamwork/apps/scrumboard/models.py
+++ teamwork/apps/scrumboard/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    tittle = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+
+class Task(models.Model):
+    assigned = models.BooleanField(default=False)
+    category = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
