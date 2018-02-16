f85cb5c3535c3f215b6b1030be88ebcfbee768e1
Ryan Monroe <rmonroe@ucsc.edu>
2018-02-16 03:29:40 -0800

    Comment out Django Toolbar

--- teamwork/urls.py
+++ teamwork/urls.py
@@ -162,8 +162,10 @@ urlpatterns = [
 
         ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-   import debug_toolbar
-   urlpatterns += [
-       url(r'^__debug__/', include(debug_toolbar.urls)),
-   ]
+#Django Toolbar Uncomment to turn on
+
+# if settings.DEBUG:
+#    import debug_toolbar
+#    urlpatterns += [
+#        url(r'^__debug__/', include(debug_toolbar.urls)),
+#    ]
