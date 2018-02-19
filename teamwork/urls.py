182858bd230509f9a5bf2ca85ce67be3ee57e54c
Ryan Monroe <rmonroe@ucsc.edu>
2018-02-19 15:33:40 -0800

    Finish Optimization need to rework match, so untouched

--- teamwork/urls.py
+++ teamwork/urls.py
@@ -163,9 +163,9 @@ urlpatterns = [
         ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #Django Toolbar Uncomment to turn on
-
-if settings.DEBUG:
-   import debug_toolbar
-   urlpatterns += [
-       url(r'^__debug__/', include(debug_toolbar.urls)),
-   ]
+#
+# if settings.DEBUG:
+#    import debug_toolbar
+#    urlpatterns += [
+#        url(r'^__debug__/', include(debug_toolbar.urls)),
+#    ]
