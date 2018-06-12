85e993585ec1f0d101dfc48109d94779617e41fd
Kevin Pannell <pannellk69@yahoo.com>
2018-06-11 21:31:30 -0700

    can now input a donation by the pennies ;)

--- teamwork/apps/core/forms.py
+++ teamwork/apps/core/forms.py
@@ -11,3 +11,20 @@ class UploadCSVForm(forms.Form):
         super(UploadCSVForm, self).__init__(*args, **kwargs)
 
     csv_file = forms.FileField()
+
+"Form to handle donations/payments"
+class PaymentForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(PaymentForm, self).__init__(*args, **kwargs)
+
+    amount = forms.DecimalField(
+        # Max amount of digits
+        max_digits=6,
+        # Two decimal places for Currency
+        decimal_places=2)
+
+    memo = forms.CharField(
+        # Text input
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        # Max length of 255 characters
+        max_length=255)
--- teamwork/apps/core/views/PaymentView.py
+++ teamwork/apps/core/views/PaymentView.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 import os
 import stripe
 from django.conf import settings
+from teamwork.apps.core.forms import PaymentForm
 
 """
 Simple view only accessible via the URL to donate/accept payments temporarily
@@ -9,16 +10,27 @@ Simple view only accessible via the URL to donate/accept payments temporarily
 
 def payment(request):
 
+    paymentForm = PaymentForm()
 
     if (request.method == "POST"):
+        paymentForm = PaymentForm(request.POST)
+
+        if not paymentForm.is_valid():
+            return
+
+        amount = request.POST.get('amount')
+        memo = request.POST.get('memo')
+
         stripe.api_key = settings.STRIPE_API_KEY
 
         # Token is created using Checkout or Elements
         # Get the payment token ID submitted by the form
         token = request.POST['stripeToken']
 
+        # TODO: convert amount from dollars to pennies if needed. Better yet, add a mask to the form to force decimal entry
+
         charge = stripe.Charge.create(
-            amount=10000, # this is pennies?
+            amount=amount, # this is pennies?
             currency='usd',
             description='Donation',
             source=token,
@@ -27,4 +39,4 @@ def payment(request):
         # TODO: validate that the payment was successful
         return render(request, 'core/success_payment.html')
 
-    return render(request, 'core/payment.html')
+    return render(request, 'core/payment.html', {'paymentForm':paymentForm})
