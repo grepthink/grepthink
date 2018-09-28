74156fe081b8222fe3c1492d4d1d748b3dbaf6c6
Kevin Pannell <kapannel@ucsc.edu>
2018-09-28 13:35:43 -0700

    added import httpresponse to EditScheduleView

--- teamwork/apps/profiles/views/EditScheduleView.py
+++ teamwork/apps/profiles/views/EditScheduleView.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Model Imports
 from teamwork.apps.profiles.models import Profile, Events
