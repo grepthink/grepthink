5344798e5dfaff97d48eea26b39da7c0527d0518
anishatavva <Priyanka>
2019-02-06 13:09:06 -0800

    added ui changes in order to display technologies

--- teamwork/apps/projects/forms.py
+++ teamwork/apps/projects/forms.py
@@ -119,9 +119,9 @@ class CreateProjectForm(forms.ModelForm):
 
 
     # desired_techs = forms.CharField(
-    #      widget=forms.TextInput(attrs={'class': 'form-control'}),
-    #      max_length=255,
-    #      required=False)
+    #       widget=forms.TextInput(attrs={'class': 'form-control'}),
+    #       max_length=255,
+    #       required=False)
 
     course = forms.ModelChoiceField(
         widget=forms.RadioSelect,
