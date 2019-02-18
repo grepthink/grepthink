c4a1f5d2693aae301bfa6198693338a007a04b74
Hiep Nguyen <hnguye43@ucsc.edu>
2019-02-18 07:22:44 -0800

    let user view other people schedule

--- teamwork/apps/profiles/views/ViewSchedule.py
+++ teamwork/apps/profiles/views/ViewSchedule.py
@@ -13,6 +13,22 @@ from django.http import HttpResponse
 from teamwork.apps.profiles.models import Profile, Events
 from teamwork.apps.projects.models import dayofweek
 
+import json
+
 def load_schedule(request,username):
-   
-    return render(request,'profiles/view_schedule.html',{'page_username':username})
+    user= get_object_or_404(User,username=username)
+    page_name="View Schedule"
+    page_description= "Viewing %s's Schedule"%(username)
+    title="View Schedule"
+    profile = Profile.objects.filter(user__username=username).first()
+    print(profile)
+
+    readable = ""
+    if profile.jsonavail:
+        jsonDec = json.decoder.JSONDecoder()
+        readable = jsonDec.decode(profile.jsonavail)
+
+    meetings = mark_safe(profile.jsonavail)
+    print(meetings)
+
+    return render(request,'profiles/view_schedule.html',{'page_username':username,'page_name' : page_name, 'page_description': page_description, 'title': title, 'json_events' : meetings})
