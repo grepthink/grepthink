bfddd06adc11868d90cf5ad43196490c557f0c99
Hiep Nguyen <Hnguyen1997@users.noreply.github.com>
2019-02-21 14:44:09 -0800

    Merge pull request #1 from Hnguyen1997/Anisha
    Anisha

--- teamwork/apps/projects/admin.py
+++ teamwork/apps/projects/admin.py
@@ -15,15 +15,21 @@ admin.site.register(Techs)
 >>>>>>> 33b79e9... fixing env problem
 =======
 
+<<<<<<< HEAD
 >>>>>>> 7e7f500... Merge pull request #2 from Hnguyen1997/Nikki
 =======
 
+=======
+>>>>>>> a88ad43... Merge pull request #1 from Hnguyen1997/Anisha
 admin.site.unregister(Techs)
 admin.site.register(Techs)
 
 
 
+<<<<<<< HEAD
 >>>>>>> 9f4e50f... Merge branch 'master' into Anisha
+=======
+>>>>>>> a88ad43... Merge pull request #1 from Hnguyen1997/Anisha
 admin.site.register(Membership)
 admin.site.register(ProjectUpdate)
 admin.site.register(ResourceUpdate)
