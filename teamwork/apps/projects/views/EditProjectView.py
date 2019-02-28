ce15d4e94aae7ee25052e56f2333d2ec73e74373
Shreya Reddy <shreyareddy@eduroam-169-233-175-30.ucsc.edu>
2019-02-28 14:52:04 -0800

    made changes to error message

--- teamwork/apps/projects/views/EditProjectView.py
+++ teamwork/apps/projects/views/EditProjectView.py
@@ -118,7 +118,8 @@ def edit_project(request, slug):
             if (not course in mem_courses):
                 messages.warning(request, "User failed to be added to the project. " + mem_to_add.username + " is not enrolled in the course")
                 # return False
-            #messages.add_message(request, messages.WARNING, "Student(s) is already added to the project.") 
+            else:
+                messages.add_message(request, messages.WARNING, "Student(s) is already added to the project.") 
 
         return redirect(view_one_project, project.slug)
 
