cddddc8095f2ee6f93c2fa4cec954827cad9da85
czhao35 <czhao35@ucsc.edu>
2019-03-08 23:24:46 -0800

    add initiator fail, re-update the work one

--- teamwork/apps/profiles/views/EditScheduleView.py
+++ teamwork/apps/profiles/views/EditScheduleView.py
@@ -12,16 +12,20 @@ from teamwork.apps.profiles.models import Profile
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.http import HttpResponseRedirect
 =======
 =======
 =======
 >>>>>>> 74b7e37... clear button and refresh page solve conflict 1
+=======
+>>>>>>> f141234... add initiator fail, re-update the work one
 # Be careful for this one, it may effect the Refresh feature
 from django.http import HttpResponseRedirect
 >>>>>>> 0625dda... clear button and refresh page for master brunch
 # For Refresh feature step 12
 from django.http import HttpResponse, JsonResponse
+<<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> e63de6a... refresh feature upload and bug fix 1
 =======
@@ -32,6 +36,8 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponseRedirect
 >>>>>>> 8ad8e4539ae5016ba9bdd6a151de46e2d912abde
 >>>>>>> 74b7e37... clear button and refresh page solve conflict 1
+=======
+>>>>>>> f141234... add initiator fail, re-update the work one
 
 # Model Imports
 from teamwork.apps.profiles.models import Profile, Events,Credentials
@@ -154,6 +160,7 @@ def save_event(request, username):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> ce379ee... Merge pull request #13 from Hnguyen1997/hiep-staging
 @login_required
@@ -213,6 +220,8 @@ def export_schedule(request,username):
 =======
 =======
 >>>>>>> 74b7e37... clear button and refresh page solve conflict 1
+=======
+>>>>>>> f141234... add initiator fail, re-update the work one
 # For Refresh feature step 13
 @csrf_exempt
 def refresh_schedule(request, username):
@@ -302,8 +311,6 @@ def get_credentials(username):
 >>>>>>> ce379ee... Merge pull request #13 from Hnguyen1997/hiep-staging
 =======
 
-=======
->>>>>>> 8ad8e4539ae5016ba9bdd6a151de46e2d912abde
 @login_required
 def import_schedule(request,username):
     service, credentials = get_credentials(username)    #otain credentials if it's non-existed
